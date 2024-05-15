ee5e085d1de878c27f0c14c1d736ec4d22e799cc
geekabhinav007 <abhinav26january@gmail.com>
2024-05-15 19:02:18 +0530

    Remove secret file

---
+++ app.js
@@ -0,0 +1,95 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const admin = require('firebase-admin');
+const port = process.env.PORT || 3000;
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+var serviceAccount = require("./key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://xingoda-e4195-default-rtdb.firebaseio.com"
+});
+
+
+let db = admin.firestore();
+
+
+
+// middlewares
+app.use(express.json({ extended: false }));
+
+app.use("/payment", require("./payment"));
+
+
+// Add a new item to the cart with the given ID because you are having uid also so only show cart items of that user whose uid matches
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+app.post('/cart/add', async (req, res) => {
+  const item = req.body;
+  const userId = req.body.uid;
+  const docRef = db.collection(`cartItems_${userId}`).doc(String(item.id));
+  await docRef.set(item);
+  res.json(item);
+});
+
+
+// Remove the card item from that user cart only
+app.post('/cart/remove', async (req, res) => {
+  let { id, userId } = req.body;
+  id = String(id);
+  userId = String(userId);
+  await db.collection(`cartItems_${userId}`).doc(id).delete();
+  res.json({ id });
+});
+
+// Clear All the cart items of that user only
+app.post('/cart/clear', async (req, res) => {
+  const userId = req.body.uid;
+  const snapshot = await db.collection(`cartItems_${userId}`).get();
+  snapshot.docs.forEach(doc => doc.ref.delete());
+  res.json({ message: 'Cart cleared' });
+});
+
+// I am having uid also so only show cart items of that user whose uid matches
+app.get('/cart', async (req, res) => {
+  const userId = req.query.uid;
+  const snapshot = await db.collection(`cartItems_${userId}`).get();
+  const items = snapshot.docs.map(doc => doc.data());
+  res.json(items);
+});
+
+
+
+
+app.post('/order/add', async (req, res) => {
+  const { uid, order } = req.body;
+  const docRef = db.collection(`orders_${uid}`).doc();
+  await docRef.set(order);
+  res.json(order);
+});
+
+
+//  show all orders of that user whose uid matches 
+app.get('/order', async (req, res) => {
+  const userId = req.query.uid;
+  const snapshot = await db.collection(`orders_${userId}`).get();
+  const orders = snapshot.docs.map(doc => doc.data());
+  res.json(orders);
+});
+
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
---
+++ payment.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const Razorpay = require("razorpay");
+const router = express.Router();
+const axios = require('axios');
+
+router.post("/orders", async (req, res) => {
+    try {
+
+
+        const totalAmount = req.body.totalPrice;
+
+
+
+
+        const instance = new Razorpay({
+            key_id: process.env.RAZORPAY_KEY_ID,
+            key_secret: process.env.RAZORPAY_SECRET,
+        });
+
+        const options = {
+            amount: totalAmount * 100,
+            currency: "INR",
+            receipt: "receipt_order_74394",
+
+        };
+
+        const order = await instance.orders.create(options);
+
+
+        if (!order) return res.status(500).send("Some error occured");
+
+        res.json(order);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+router.post("/success", async (req, res) => {
+    try {
+        // getting the details back from our font-end
+        const {
+            razorpayPaymentId,
+            razorpayOrderId,
+            cartItems,
+            uid,
+            totalPrice,
+        } = req.body;
+
+
+
+
+        // Add products to order
+        const items = cartItems.map(item => {
+            const selectedPlan = item.selectedPlan;
+            let rent;
+            if (selectedPlan === 'day') {
+                rent = item.rentPerDay;
+            } else if (selectedPlan === 'month') {
+                rent = item.rentPerMonth;
+            } else {
+                rent = item.rentPerYear;
+            }
+            const itemTotalPrice = Math.round(Number(rent) + Number(item.securityDeposit));
+            return {
+                image: item.image,
+                description: item.description,
+                selectedPlan: selectedPlan,
+                uid: item.uid,
+                price: item.price,
+                name: item.name,
+                id: item.id,
+                availabilityPlace: item.availabilityPlace,
+                categories: item.categories,
+                rent: rent,
+                itemTotalPrice: itemTotalPrice,
+            };
+        });
+        
+
+        const order = {
+            items,
+            paymentId: razorpayPaymentId,
+            orderId: razorpayOrderId,
+            date: new Date().toLocaleString(),
+            orderPrice: Number(totalPrice/100),
+        };
+        await axios.post('http://localhost:4000/order/add', { uid, order });
+
+
+        res.json({
+            msg: "success",
+            orderId: razorpayOrderId,
+            paymentId: razorpayPaymentId,
+        });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+module.exports = router;
